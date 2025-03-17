3397c44b78facede09222370446d7c2ab6a2609a
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-17 15:59:26 +0300

    feat: integrate inertiajs/vue to example application

--- example/resources/js/app.js
+++ example/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createInertiaApp } from "@inertiajs/vue3"
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createApp, h } from "vue"
+
+void createInertiaApp({
+    resolve: name => resolvePageComponent(
+        `./pages/${name}.vue`,
+        import.meta.glob("./pages/**/*.vue"),
+    ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
--- example/vite.config.js
+++ example/vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from "@vitejs/plugin-vue";
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
         tailwindcss(),
     ],
