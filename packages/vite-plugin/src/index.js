5df990e7766e9859d0fa491a4d7c65a150fdfb91
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-18 02:01:19 +0300

    feat: create a vite plugin for automatically generating the lang file

---
+++ packages/vite-plugin/src/index.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+import { exec } from "node:child_process";
+
+export default (config) => {
+    const cmd = ['php', 'artisan', 'lang:generate'];
+
+    if (config && config.path) {
+        cmd.push(config.path);
+    }
+
+    const runCommand = () => {
+        exec(cmd.join(" "), (error, stdout, stderr) => {
+            if (error) {
+                console.error(`[vite-plugin-laravel-lang] error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`[vite-plugin-laravel-lang] stderr: ${stderr}`);
+                return;
+            }
+            console.log(`[vite-plugin-laravel-lang] ${stdout}`);
+        });
+    }
+
+    return {
+        name: "vite-plugin-laravel-lang",
+        buildStart: async () => {
+            exec(`ps -o args= -p ${process.pid}`, (error, stdout, stderr) => {
+                if (stdout.trim().startsWith("sail")) {
+                    // Replace php with sail in cmd
+                    cmd[0] = "sail";
+                }
+                runCommand();
+            });
+        },
+        handleHotUpdate({ file }) {
+            const langPath = path.join(process.cwd(), "lang");
+
+            if (file.startsWith(langPath) && (file.endsWith(".php") || file.endsWith(".json"))) {
+                runCommand();
+            }
+        },
+    };
+}
