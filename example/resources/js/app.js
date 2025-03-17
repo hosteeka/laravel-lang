e6dccd35acc0273e6a1146d4d8b1c891fecd808a
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-17 19:51:51 +0300

    feat: add LangVue plugin

--- example/resources/js/app.js
+++ example/resources/js/app.js
@@ -1,6 +1,8 @@
 import { createInertiaApp } from "@inertiajs/vue3"
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createApp, h } from "vue"
+import { LangVue } from "laravel-lang";
+import { Lang } from "./lang.js";
 
 void createInertiaApp({
     resolve: name => resolvePageComponent(
@@ -10,6 +12,7 @@ void createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(LangVue, Lang)
             .mount(el)
     },
-})
+});
