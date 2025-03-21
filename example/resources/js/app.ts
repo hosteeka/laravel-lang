4d71c37edc23914ae2ebec6334c22c94e8b9ae71
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-22 00:44:56 +0300

    chore: migrate example to TypeScript and update related files

--- example/resources/js/app.ts
+++ example/resources/js/app.ts
@@ -1,13 +1,13 @@
 import { createInertiaApp } from "@inertiajs/vue3"
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { createApp, h } from "vue"
+import { createApp, DefineComponent, h } from "vue"
 import { LangVue } from "laravel-lang";
 import { Lang } from "./lang.js";
 
 void createInertiaApp({
     resolve: name => resolvePageComponent(
         `./pages/${name}.vue`,
-        import.meta.glob("./pages/**/*.vue"),
+        import.meta.glob<DefineComponent>("./pages/**/*.vue"),
     ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
--- example/vite.config.js
+++ example/vite.config.js
@@ -3,12 +3,12 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 import { defineConfig } from 'vite';
-import laravelLang from './vendor/hosteeka/laravel-lang/packages/vite-plugin';
+import runCommands from 'vite-plugin-run';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
         vue({
@@ -19,12 +19,18 @@ export default defineConfig({
                 },
             },
         }),
-        laravelLang(),
         tailwindcss(),
+        runCommands([
+            {
+                name: 'laravel-lang',
+                run: ['php', 'artisan', 'lang:generate'],
+                pattern: ['lang/**/*'],
+            },
+        ]),
     ],
     resolve: {
         alias: {
-            "laravel-lang": path.resolve("vendor/hosteeka/laravel-lang"),
+            'laravel-lang': path.resolve('vendor/hosteeka/laravel-lang'),
         }
     }
 });
